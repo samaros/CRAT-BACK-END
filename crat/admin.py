99c7ad2f2ded8cfdd3436c6a29b2118f1d116219
yulian-khalitov <khalitov.yulian@yandex.ru>
2021-09-28 16:33:59 +0300

    feature: admin panel

---
+++ crat/admin.py
@@ -0,0 +1,33 @@
+import csv
+from django.contrib import admin
+from crat.models import Investor
+from django.http import HttpResponse
+
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export selected investors as CSV"
+
+
+class InvestorAdmin(admin.ModelAdmin, ExportCsvMixin):
+    list_display = (
+        'email',
+        'address',
+    )
+    actions = ["export_as_csv"]
+
+
+admin.site.register(Investor, InvestorAdmin)
